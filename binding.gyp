d96a4b0fe3fe2112ca8cd4006a62bd6548a27ab0
geta6 <getakura@gmail.com>
2013-06-06 09:17:29 +0900

    compatible with v0.10.x

--- binding.gyp
+++ binding.gyp
@@ -1,12 +1,9 @@
 {
-'targets':[
+  'targets':[
     {
-    'target_name':'mecab',
-    'sources':['mecab.cc'],
-    'libraries':[
-        '/usr/local/lib/libmecab.dylib'
-        ]
+      'target_name':'mecab',
+      'sources':['mecab.cc'],
+      'libraries':[ '-lmecab' ]
     }
-    
-]
+  ]
 }
--- mecab.cc
+++ mecab.cc
@@ -1,33 +1,33 @@
-#define BUILDING_NODE_EXTENSION
+//#define BUILDING_NODE_EXTENSION
 //#include </usr/local/Cellar/node/0.6.19/include/node/uv.h>
 //#include <uv.h>
-#include <v8.h>
+//#include <v8.h>
 #include <node.h>
 #include <iostream>
 #include <mecab.h>
 //#include "node-mecab.h"
 
 using namespace node;
-using namespace v8; 
+using namespace v8;
 //using namespace std;
 
 // We use a struct to store information about the asynchronous "work request".
 struct Baton {
     // libuv's request struct.
     uv_work_t request;
-    
+
     // This handle holds the callback function we'll call after the work request
     // has been completed in a threadpool thread. It's persistent so that V8
     // doesn't garbage collect it away while our request waits to be processed.
     // This means that we'll have to dispose of it later ourselves.
     Persistent<Function> callback;
-    
+
     // Tracking errors that happened in the worker function. You can use any
     // variables you want. E.g. in some cases, it might be useful to report
     // an error number.
     bool error;
     std::string error_message;
-    
+
     // Custom data you can pass through.
     const char* result;
     const char* parseString;
@@ -42,14 +42,14 @@ void AsyncAfter(uv_work_t* req);
 // work request object and schedules it for execution.
 Handle<Value> Async(const Arguments& args) {
     HandleScope scope;
-    
+
     if (!args[1]->IsFunction() && !args[0]->IsString() ) {
         return ThrowException(Exception::TypeError(
                                                    v8::String::New("First argument must be a callback function")));
     }
     // There's no ToFunction(), use a Cast instead.
     Local<Function> callback = Local<Function>::Cast(args[1]);
-    
+
     // This creates our work request, including the libuv struct.
     Baton* baton = new Baton();
     baton->error = false;
@@ -57,13 +57,13 @@ Handle<Value> Async(const Arguments& args) {
     baton->request.data = baton;
     baton->parseString = *String::Utf8Value(args[0]->ToString());
     baton->callback = Persistent<Function>::New(callback);
-    
+
     // Schedule our work request with libuv. Here you can specify the functions
     // that should be executed in the threadpool and back in the main thread
     // after the threadpool function completed.
-    int status = uv_queue_work(uv_default_loop(),&baton->request , AsyncWork, AsyncAfter);
+    int status = uv_queue_work(uv_default_loop(),&baton->request , AsyncWork, (uv_after_work_cb)AsyncAfter);
     assert(status == 0);
-    
+
     return Undefined();
 }
 // This function is executed in another thread at some point after it has been
@@ -75,13 +75,13 @@ void AsyncWork(uv_work_t* req) {
     Baton* baton = static_cast<Baton*>(req->data);
 
     // Do work in threadpool here.
-    
+
     //MeCabの中身を書いてく。
     MeCab::Tagger *t = MeCab::createTagger("");
     //String::Utf8Value input(baton->request.data);
     const char *result = t->parse(baton->parseString);
     baton->result = result;
-    
+
     // If the work we do fails, set baton->error_message to the error string
     // and baton->error to true.
 }
@@ -91,14 +91,14 @@ void AsyncWork(uv_work_t* req) {
 void AsyncAfter(uv_work_t* req) {
     HandleScope scope;
     Baton* baton = static_cast<Baton*>(req->data);
-    
+
     if (baton->error) {
         Local<Value> err = Exception::Error(v8::String::New(baton->error_message.c_str()));
-        
+
         // Prepare the parameters for the callback function.
         const unsigned argc = 1;
         Local<Value> argv[argc] = { err };
-        
+
         // Wrap the callback function call in a TryCatch so that we can call
         // node's FatalException afterwards. This makes it possible to catch
         // the exception from JavaScript land using the
@@ -118,7 +118,7 @@ void AsyncAfter(uv_work_t* req) {
             Local<Value>::New(Null()),
             Local<Value>::New(v8::String::New(baton->result))
         };
-        
+
         // Wrap the callback function call in a TryCatch so that we can call
         // node's FatalException afterwards. This makes it possible to catch
         // the exception from JavaScript land using the
@@ -129,7 +129,7 @@ void AsyncAfter(uv_work_t* req) {
             node::FatalException(try_catch);
         }
     }
-    
+
     // The callback is a permanent handle, so we have to dispose of it manually.
     baton->callback.Dispose();
     delete baton;
@@ -157,7 +157,7 @@ Handle<Value> Tagger::Parse (const Arguments& args) {
 	HandleScope scope;
 	if (args.Length() != 1 || !args[0]->IsString()) {
 		return ThrowException(Exception::Error(String::New("Bad argument.")));
-	}   
+	}
 
 	Tagger *self = ObjectWrap::Unwrap<Tagger>(args.This());
 
